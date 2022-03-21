5e39309d40f7bd1d1ac32490b6210f4bf370bee6
anagaby2196 <80338672+anagaby2196@users.noreply.github.com>
2022-03-20 23:13:43 -0600

    Se agregaron las carpetas faltantes

--- ProyectoFinal/BE.API/Mapping/MappingProfile.cs
+++ ProyectoFinal/BE.API/Mapping/MappingProfile.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-//using data = BE.DAL.DO.Objects;
+using data = BE.DAL.DO.Objects;
 
 namespace BE.API.Mapping
 {
@@ -11,8 +11,16 @@ namespace BE.API.Mapping
     {
         public MappingProfile()
         {
-            //CreateMap<data.Categories, DataModels.Categories>().ReverseMap();
-            //CreateMap<data.Products, DataModels.Products>().ReverseMap();
+            CreateMap<data.Citas, DataModels.Citas>().ReverseMap();
+            CreateMap<data.CitasProgramadas, DataModels.CitasProgramadas>().ReverseMap();
+            CreateMap<data.Direccion, DataModels.Direccion>().ReverseMap();
+            CreateMap<data.Doctor, DataModels.Doctor>().ReverseMap();
+            CreateMap<data.Expediente, DataModels.Expediente>().ReverseMap();
+            CreateMap<data.Paciente, DataModels.Paciente>().ReverseMap();
+            CreateMap<data.Persona, DataModels.Persona>().ReverseMap();
+            CreateMap<data.ReporteCitas, DataModels.ReporteCitas>().ReverseMap();
+            CreateMap<data.TipoUsuario, DataModels.TipoUsuario>().ReverseMap();
+            CreateMap<data.Usuarios, DataModels.Usuarios>().ReverseMap();
         }
     }
 }
