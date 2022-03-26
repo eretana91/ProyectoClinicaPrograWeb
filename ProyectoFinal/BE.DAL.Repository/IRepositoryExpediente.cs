26c0ad5a2cf8fab86a144d9bf72eca1e566455b0
anagaby2196 <80338672+anagaby2196@users.noreply.github.com>
2022-03-26 11:02:11 -0600

    Agregar información en IRepositoryExpediente y RepositoryExpediente

--- ProyectoFinal/BE.DAL.Repository/IRepositoryExpediente.cs
+++ ProyectoFinal/BE.DAL.Repository/IRepositoryExpediente.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using data = BE.DAL.DO.Objects;
 
 namespace BE.DAL.Repository
 {
-    interface IRepositoryExpediente
+    public interface IRepositoryExpediente : IRepository<data.Expediente>
     {
+        Task<IEnumerable<data.Expediente>> GetAllAsync();
+        Task<data.Expediente> GetOneByIdAsync(int id);
     }
 }
--- ProyectoFinal/BE.DAL.Repository/RepositoryExpediente.cs
+++ ProyectoFinal/BE.DAL.Repository/RepositoryExpediente.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using BE.DAL.DO.Objects;
+using BE.DAL.EF;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using data = BE.DAL.DO.Objects;
+
 
 namespace BE.DAL.Repository
 {
-    class RepositoryExpediente
+    public class RepositoryExpediente : Repository<data.Expediente>, IRepositoryExpediente
     {
+        public RepositoryExpediente(NDbContext _dbContext) : base(_dbContext)
+        {
+
+        }
+
+        public Task<IEnumerable<Expediente>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Expediente> GetOneByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private NDbContext _db
+        {
+            get
+            {
+                return dbContext as NDbContext;
+            }
+        }
     }
 }
