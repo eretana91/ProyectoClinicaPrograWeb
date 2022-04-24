41a7f0c03febb9d1300db46c46826dcae4d31ee4
eretana91 <eretana91@gmail.com>
2022-04-23 23:58:06 -0600

    Creacionde Vistas
    Vistas de listados para Expedientes, Pagos, usuarios e Inventario

---
+++ Proyecto_Version_Final/Proyecto_FrontEnd/FE/Controllers/ExpedientesController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FE.Models;
+using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace FE.Controllers
+{
+    public class ExpedientesController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            //HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri(GlobalVariables.strUri);
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //    new MediaTypeWithQualityHeaderValue("application/json"));
+            //HttpResponseMessage response = client.GetAsync("api/Categoria").Result;
+
+            List<Expedientes> ListaExpediente = new List<Expedientes>();
+           Expedientes expediente = new Expedientes();
+            expediente.cedula = "114830553";
+            //expediente.fechaN = (2021,04,02);
+            expediente.ciudad = "Heredia";
+            expediente.canton = "Heredia";
+            expediente.distrito = "Mercedes Norte";
+            expediente.diagnostico = "N/A";
+            expediente.antecendente = "N/A";
+            expediente.mediUtilizados = "N/A";
+            expediente.anteQuirurgicos = "N/A";
+            expediente.fracturas = "N/A";
+            expediente.anteFamiliares = "N/A";
+        
+
+            ListaExpediente.Add(expediente);
+
+            //if (response.IsSuccessStatusCode)
+            if (true)
+            {
+                ViewBag.result = ListaExpediente;
+                //ViewBag.result = response.Content.ReadAsAsync<List<Categoria>>().Result;
+            }
+            //else
+            //{
+            //    ViewBag.result = "Error";
+            //}
+                       
+                         
+            return View();
+        }
+
+        public ActionResult EditarUsuario(int cedula)
+        {
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(GlobalVariables.strUri);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //HttpResponseMessage response = client.GetAsync("api/Categoria/" + cedula.ToString()).Result;
+
+            //if (!response.IsSuccessStatusCode)
+            //{
+            //    ViewBag.result = "Error";
+            //}
+
+            return View();
+
+            //return View(response.Content.ReadAsAsync<Categoria>().Result);
+        }
+
+        //public ActionResult EditarUsuario(int cedula)
+        //{
+
+        //    HttpClient client = new HttpClient();
+        //    client.BaseAddress = new Uri(GlobalVariables.strUri);
+        //    client.DefaultRequestHeaders.Accept.Clear();
+        //    client.DefaultRequestHeaders.Accept.Add(
+        //        new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //    //HttpResponseMessage response = client.GetAsync("api/Categoria/" + cedula.ToString()).Result;
+
+        //    //if (!response.IsSuccessStatusCode)
+        //    //{
+        //    //    ViewBag.result = "Error";
+        //    //}
+
+        //    return View();
+
+        //    //return View(response.Content.ReadAsAsync<Categoria>().Result);
+        //}
+
+
+
+    }
+}
---
+++ Proyecto_Version_Final/Proyecto_FrontEnd/FE/Models/Pagos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FE.Models
+{
+    public partial class Pagos
+    {
+        public int? idPago { get; set; }
+        public int? monto { get; set; }
+        public string banco { get; set; }
+        public string cedula { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public Nullable<System.DateTime> fechaPago { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string notas { get; set; }
+        public int? tipoPago { get; set; }
+    }
+}
